073cf7a59b5f76fd86de35d1ea44347bc5bc9d76
henry232323 <henry@typeus.me>
2019-12-03 22:19:15 -0700

    Prob 4

---
+++ 4/prob04b.py
@@ -0,0 +1,22 @@
+mx = 765869
+mn = 234208
+
+def is_valid(n):
+    n = "X" + str(n) + "X"
+    for x in range(1, len(n)-2):
+        if n[x] == n[x + 1] and n[x] != n[x + 2] and n[x] != n[x-1]:
+            break
+    else:
+        return False
+
+    for x in range(1, len(n)-2):
+        if int(n[x]) > int(n[x+1]):
+            return False
+
+    return True
+
+ct = 0
+for i in range(mn, mx+1):
+    ct += is_valid(i)
+
+print(ct)
